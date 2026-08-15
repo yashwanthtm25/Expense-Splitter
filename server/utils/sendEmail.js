b79bec11454e3394cba71be2e31027d3c734749a
yashwanthtm25 <yashwanthtm363@gmail.com>
2026-08-15 16:37:58 +0530

    changing nodemailer with google service to resend http api

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,23 +1,22 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
+    const { data, error } = await resend.emails.send({
+      from: "Expense Splitter <onboarding@resend.dev>",
+      to: [to],
       subject,
       html,
     });
 
-    console.log("Email sent successfully");
+    if (error) {
+      console.error("Email sending failed:", error);
+      throw new Error(error.message);
+    }
+
+    console.log("Email sent successfully:", data.id);
   } catch (error) {
     console.error("Email sending failed:", error.message);
     throw error;
